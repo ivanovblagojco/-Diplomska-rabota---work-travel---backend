897c98492f964f0cece357a928ff835fb4243488
Blagojco <ivanovblagojco@gmail.com>
2021-04-24 20:47:08 +0200

    Added request body to the controller

--- WorkAndTravel/src/main/java/com/diplomska/backend/web/ApplicationController.java
+++ WorkAndTravel/src/main/java/com/diplomska/backend/web/ApplicationController.java
@@ -4,6 +4,7 @@ import com.diplomska.backend.helpers.ApplicationHelper;
 import com.diplomska.backend.model.Application;
 import com.diplomska.backend.service.interfaces.ApplicationService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class ApplicationController {
     }
 
     @PostMapping("/createApplication")
-    public Application createApplication(ApplicationHelper application){
+    public Application createApplication(@RequestBody ApplicationHelper application){
         return this.applicationService.createApplication(application);
     }
 
